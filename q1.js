aa6eb95da555a128fbb88d3c7dfe30695adbbfb5
Inbar Weinberg <55811314+Inbar-Weinberg@users.noreply.github.com>
2021-01-19 14:38:34 +0200

    Q1 complete

--- q1.js
+++ q1.js
@@ -2,10 +2,9 @@ function run() {
     const input = document.getElementById("input").value;
     const output = document.getElementById("output");
     let out = "";
-/**/
-    // write your code here
-    // out = input + 5;
-
-/**/
+    /**/
+    let temp = JSON.parse (temp);
+    out = Array.isArray(temp);
+    /**/
     output.innerText = out;
 }
